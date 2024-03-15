9eef1efd3e9a82750e351745047a535bf677f50a
sarazayan988@gmail.com <sarazayan988@gmail.com>
2024-03-15 18:53:03 +0200

    create database models and migrations

--- E-Commerce/E-Commerce.Domain/obj/Debug/net8.0/E-Commerce.Domain.AssemblyInfo.cs
+++ E-Commerce/E-Commerce.Domain/obj/Debug/net8.0/E-Commerce.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e25b18dd1b7f71a495f6aca5953f8456c1d896e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

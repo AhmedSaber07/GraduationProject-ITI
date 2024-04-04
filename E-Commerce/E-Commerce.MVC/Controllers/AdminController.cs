73bec31631db6e960a118c18aa43e880c4c282cf
Sara Mohammed <57673665+Saramohamed13@users.noreply.github.com>
2024-04-04 06:17:41 +0200

    Reset Password cycle front & back in mvc

--- E-Commerce/E-Commerce.MVC/Controllers/AdminController.cs
+++ E-Commerce/E-Commerce.MVC/Controllers/AdminController.cs
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.Identity;
 using NuGet.Common;
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
 
 public class AdminController : Controller
 {
@@ -21,9 +24,98 @@ public class AdminController : Controller
         _httpClient.DefaultRequestHeaders.Accept.Clear();
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     }
-  
-    
 
+	public ActionResult ResetPassword()
+	{
+		return View();
+	}
+	
+    [HttpPost("sendCode")]
+	public async Task< ActionResult> sendCode(string email)
+    {
+        HttpContext.Session.SetString("resEmail", email);
+        var apiUrl = $"api/UserAccount/SendCode?email={Uri.EscapeDataString(email)}";
+
+		HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, null);
+
+		if (response.IsSuccessStatusCode)
+		{
+			return View("EnterCode");
+		}
+		else
+		{
+			return RedirectToAction("ResetPassword");
+		}
+
+	}
+    public async Task<ActionResult> EnterCode( )
+    {
+        return View();
+    }
+    [HttpPost("EnterCode")]
+    public async Task<ActionResult> EnterCode(string input1, string input2, string input3, string input4)
+    {
+        string email = HttpContext.Session.GetString("resEmail");
+        string scode = input1 + input2 + input3 + input4;
+        int code = int.Parse(scode);
+
+        var apiUrl = $"/api/UserAccount/CheckCode?code={code}&email={email}";
+
+        HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, null);
+
+
+        if (response.IsSuccessStatusCode)
+        {
+
+            bool isSuccess = await response.Content.ReadFromJsonAsync<bool>();
+
+            if (isSuccess)
+            {
+                return RedirectToAction("EnterNewPassword");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Code Not Correct";
+                return View("EnterCode");
+            }
+        }
+        else
+        {
+            ViewBag.ErrorMessage = "Code Not Correct";
+            return View("EnterCode");
+        }
+
+
+
+    }
+    public async Task<ActionResult> EnterNewPassword()
+	{
+		return View();
+	}
+    [HttpPost("EnterNewPassword")]
+    public async Task<ActionResult> EnterNewPassword(string Password)
+    {
+            var apiUrl = "api/UserAccount/NewResetPassword";
+        string email = HttpContext.Session.GetString("resEmail");
+            var requestContent = new StringContent(JsonConvert.SerializeObject(new
+            {
+                Email = email,
+                NewPassword = Password
+            }), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, requestContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+            return RedirectToAction("Login", "Admin");
+             }
+            else
+            {
+              return View();
+
+            }
+        
+    }
     public ActionResult AddAddress()
     {
         return View();
@@ -38,7 +130,7 @@ public class AdminController : Controller
         
         string token = HttpContext.Session.GetString("AuthToken");
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-        var jsonContent = JsonSerializer.Serialize(addressDto);
+        var jsonContent = System.Text.Json.JsonSerializer.Serialize(addressDto);
         var stringContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
        
         var response = await _httpClient.PostAsync("api/UserAccount/AddAddress", stringContent);
@@ -54,15 +146,6 @@ public class AdminController : Controller
             return View("Error404");
         }
     }
-
-    [HttpPut("{oldEmail}/UpdateEmail")]
-    public async Task<IActionResult> UpdateEmail(string oldEmail, string newEmail)
-    {
-        var response = await _httpClient.PutAsync($"api/UserAccount/{oldEmail}/UpdateEmail?newEmail={newEmail}", null);
-
-        return await HandleResponse(response);
-    }
-
     [HttpPut("{oldPhone}/UpdatePhone")]
     public async Task<IActionResult> UpdatePhone(string oldPhone, string newPhone)
     {
@@ -70,7 +153,6 @@ public class AdminController : Controller
 
         return await HandleResponse(response);
     }
-
     [HttpPost]
     public async Task<IActionResult> Logout()
     {
@@ -85,12 +167,12 @@ public class AdminController : Controller
     [HttpPost("login")]
     public async Task<ActionResult> Login(LoginDto loginDtoo)
     {
-        var jsonContent = JsonSerializer.Serialize(loginDtoo);
+        var jsonContent = System.Text.Json.JsonSerializer.Serialize(loginDtoo);
         var stringContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
         var response = await _httpClient.PostAsync("api/UserAccount/login", stringContent);
         string responseContent = await response.Content.ReadAsStringAsync();
-        var LoginDto = JsonSerializer.Deserialize<LoginResponse>(responseContent);
+        var LoginDto = System.Text.Json.JsonSerializer.Deserialize<LoginResponse>(responseContent);
         
         if (response.IsSuccessStatusCode)
         {
@@ -119,7 +201,7 @@ public class AdminController : Controller
     [HttpPost("reset-password")]
     public async Task<IActionResult> ResetPassword(ResetPasswordDto resetPasswordDto)
     {
-        var jsonContent = JsonSerializer.Serialize(resetPasswordDto);
+        var jsonContent = System.Text.Json.JsonSerializer.Serialize(resetPasswordDto);
         var stringContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
         var response = await _httpClient.PostAsync("api/UserAccount/reset-password", stringContent);

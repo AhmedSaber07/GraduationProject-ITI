f7ddc84a2fb190ce10f50862b1f4543220198757
Mohammed Khaled <64231779+mohammed277@users.noreply.github.com>
2024-04-05 07:45:59 +0200

    Update OrderController.cs

--- E-Commerce/E-Commerce.WebAPI/Controllers/OrderController.cs
+++ E-Commerce/E-Commerce.WebAPI/Controllers/OrderController.cs
@@ -12,7 +12,7 @@ namespace E_Commerce.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class OrderController : ControllerBase
     {
         private readonly iorderService _orderservice;

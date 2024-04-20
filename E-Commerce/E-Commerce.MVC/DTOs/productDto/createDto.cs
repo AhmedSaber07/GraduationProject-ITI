54bef5596bb570baf08c0c9757c85ece1cdb85cf
Mohammed Khaled <64231779+mohammed277@users.noreply.github.com>
2024-04-20 12:43:19 +0200

    Revert "Merge branch 'main' of https://github.com/AhmedSaber07/GraduationProject-ITI"
    This reverts commit 8d36499695b4a0c3b47af0bfd46a3fda7fd9fcf0, reversing
    changes made to a814fac70195fbe500642af3e753d9b55beedd1a.

--- E-Commerce/E-Commerce.MVC/DTOs/productDto/createDto.cs
+++ E-Commerce/E-Commerce.MVC/DTOs/productDto/createDto.cs
@@ -10,23 +10,12 @@ namespace E_Commerce.MVC.DTOs.productDto
 {
     public class createDto
     {
-        [Required(ErrorMessage = "Arabic name is required.")]
-        [MaxLength(100, ErrorMessage = "Arabic name cannot exceed 100 characters.")]
-        public string nameAr { get; set; }
-        [Required(ErrorMessage = "English name is required.")]
-        [MaxLength(100, ErrorMessage = "English name cannot exceed 100 characters.")]
+        public string nameAr { get; set; }      
         public string nameEn { get; set; }
-        [Required(ErrorMessage = "Arabic description is required.")]
-
         public string descriptionAr { get; set; }    
-        [Required(ErrorMessage = "Arabic color is required.")]
         public string colorAr { get; set; }  
-        [Required(ErrorMessage = "English color is required.")]
-        public string colorEn { get; set; }
-        [Required(ErrorMessage = "English description is required.")]
+        public string colorEn { get; set; }    
         public string descriptionEn { get; set; }
-        [Required(ErrorMessage = "Price is required.")]
-        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number.")]
         public decimal price { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "sorry the quantity between  0  to 2147483647")]
         [Required(ErrorMessage = "please  enter the quantity of this product in our stock")]

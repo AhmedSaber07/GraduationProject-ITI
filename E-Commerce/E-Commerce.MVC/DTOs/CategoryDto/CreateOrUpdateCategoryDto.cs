29550477ce53363bc3846aea81f4cbdf6ff5e40d
Mohammed Khaled <64231779+mohammed277@users.noreply.github.com>
2024-04-26 17:59:42 +0200

    .

--- E-Commerce/E-Commerce.MVC/DTOs/CategoryDto/CreateOrUpdateCategoryDto.cs
+++ E-Commerce/E-Commerce.MVC/DTOs/CategoryDto/CreateOrUpdateCategoryDto.cs
@@ -1,10 +1,19 @@
-﻿namespace E_Commerce.MVC.DTOs.CategoryDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.MVC.DTOs.CategoryDto
 {
     public class CreateOrUpdateCategoryDto
     {
         public Guid id { get; set; }
-        public string nameAr { get; set; }      
+        [Display(Name = "Arabic Name")]
+        [Required(ErrorMessage = " Arabic  name is required.")]
+        [MaxLength(100, ErrorMessage = "Category name cannot exceed 100 characters.")]
+        public string nameAr { get; set; }
+        [Display(Name = "English Name")]
+        [Required(ErrorMessage = " English  name is required.")]
+        [MaxLength(100, ErrorMessage = "Category name cannot exceed 100 characters.")]
         public string nameEn { get; set; }
+        [Display(Name = "Parent Category")]
         public Guid? ParentCategoryId { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? CreatedAt { get; set; }
--- E-Commerce/E-Commerce.MVC/DTOs/productDto/getProductwithImage.cs
+++ E-Commerce/E-Commerce.MVC/DTOs/productDto/getProductwithImage.cs
@@ -8,17 +8,25 @@ namespace E_Commerce.MVC.DTOs.productDto
         public Guid id { get; set; }
         [Display(Name ="Arabic Name")]
         public string nameAr { get; set; }
+        [Display(Name = "English Name")]
         public string nameEn { get; set; }
+        [Display(Name = "Arabic Description")]
         public string descriptionAr { get; set; }
-        public string colorAr { get; set; }
-        public string colorEn { get; set; }
+        [Display(Name = "English Description")]
         public string descriptionEn { get; set; }
+        [Display(Name = "Arabic Color")]
+        public string colorAr { get; set; }
+        [Display(Name = "English Color")]
+        public string colorEn { get; set; }
+        [Display(Name = "Price")]
         public decimal price { get; set; }
+        [Display(Name = "Stock Quantity")]
         public int stockQuantity { get; set; }
+        [Display(Name = "Category Name")]
         public Guid categoryId { get; set; }
         public Guid brandId { get; set; }
         public decimal rating { get; set; }
-        public virtual List<IFormFile> FormFiles { get; set; }
+        public virtual List<IFormFile>? FormFiles { get; set; }
 
         public List<string>? images { get; set; }
         public virtual ICollection<ReviewDto.GetReviewDto>? reviews { get; set; }
